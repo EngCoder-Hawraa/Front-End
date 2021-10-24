e46ac43f517c0fe7ebec7a1da85926782c3242c4
EngCoder-Hawraa <h.a.j.engineer@gmail.com>
2021-10-25 00:42:41 +0300

    HTML

---
+++ HTML/index.js
@@ -0,0 +1,17 @@
+/*
+لغة توصيف المواقع
+HTML Hyper text Markup(Tags ماركب معناها) Language يعني معناها من تضغط على اي لنك بالصفحة يروح للموقع مباشرة
+1. HTML what is HTML? & why I used?
+  هي اساس اي موقع عندك ما هو الويب سايت هي مجموعة من الفايلات و الصور الي موجودة عندك 
+https://startbootstrap.com/theme/agency
+https://startbootstrap.com/previews/agency
+https://startbootstrap.com/
+what things I need ?
+1.Browser
+2.Code Editor (VS & othes)
+
+CTRL + shift + N = new folder
+
+
+
+*/
